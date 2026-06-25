ecd9f6fcf2328cb46d79ed613111724f95bf9d1a
Samyuckkk <samyakoholkar@gmail.com>
2026-06-25 19:31:12 +0530

    index on feature/temporary: 0765614 patrially experience controller

--- portfolio/backend/src/routes/auth.routes.js
+++ portfolio/backend/src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const router = express.router()
+const router = express.Router()
 const authController = require('../controllers/auth.controllers')
 
 router.post('/admin/register', authController.registerAdmin)
 router.post('/admin/login', authController.loginAdmin)
+
+module.exports = router
--- portfolio/backend/src/routes/project.routes.js
+++ portfolio/backend/src/routes/project.routes.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const projectController = require('../controllers/project.controllers')
+const authMiddleware = require('../middlewares/auth.middleware')
 
 router.get('/', projectController.getAllProjects)
-router.delete('/delete', projectController.deleteProject)
-router.post('/create', projectController.createProject)
-router.put('/update', projectController.updateProject)
+router.delete('/delete/:id', authMiddleware.authAdminMiddleware, projectController.deleteProject)
+router.post('/create', authMiddleware.authAdminMiddleware, projectController.createProject)
+router.put('/update/:id', authMiddleware.authAdminMiddleware, projectController.updateProject)
+
+module.exports = router
